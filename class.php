466a9a3810658cb7581517870e829a3d7cc94fd4
tula <oguntulagbolaga@gmail.com>
2021-08-19 22:55:38 +0100

    add database

--- class.php
+++ class.php
@@ -3,7 +3,16 @@
 class User{
     public $dbcon;
     function __construct(){
-        $this->dbcon = new Mysqli('localhost','root','','stage2');
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db = substr($cleardb_url["path"],1);
+        $active_group = 'default';
+        $query_builder = TRUE;
+        // Connect to DB
+        // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        $this->dbcon = new Mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     }
     function addtodb($name,$email,$message){
         $sql="INSERT INTO formentry(name, email, message) VALUES('$name', '$email','$message')";
